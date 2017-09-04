d209b7582cf0be932e60532634ff87c2550fa142
olive <olive@DESKTOP-FA91UF5>
2017-09-04 18:57:37 +0200

    Now using integer arrays instead of strings. Final version for 2.2

--- RecursivePascal.java
+++ RecursivePascal.java
@@ -1,22 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class RecursivePascal { 
-	//Class for Pascal's Triangle
-	boolean UpsideDown = false;
+	//Class for Pascal's Triangle. Calculates and prints the first n lines of Pascal's Triangle, using a two-dimensional array.
+	boolean UpsideDown = false; //if set to 'true', the triangle will be decrementing (upside down)
+	ArrayList<int[]> Triangle = new ArrayList<int[]>(); 
 	
 	public void printPascal(int n){
-		//Prints line n of Pascal's Triangle and makes a recursive call to print line n-1
-		String Line = new String();
-		if (UpsideDown == false){ //Prints incrementing triangle
+		//Calculates and prints the first n lines of Pascal's Triangle
+		int[] Line = new int[n+1];
+		for (int i = 0; i <= n/2; i++){
+			Line[i] = (binom (n,i));
+			Line[n-i] = Line[i];
+		}
+		Triangle.add(Line);
 		if (n > 0){
 			printPascal(n-1);
-		}
-		for (int i = 0; i <= n; i++){
-			Line += (binom (n,i)+" "); //Appends the line string with each number
-		}
-		System.out.println(Line);
-		}
-		else { //Prints decrementing triangle	
+		} else {
+			if (UpsideDown == false) { //incrementing triangle
+				Collections.reverse(Triangle);
+			}
+			for (int[] s : Triangle) {
+			System.out.println(Arrays.toString(s));
+			}
 		}
 	}
 	
@@ -25,4 +32,9 @@ public class RecursivePascal {
 		if (k == n || k == 0) return 1;
 		return (binom(n-1,k-1) + binom (n-1,k));
 		}
+	
+	public void SetUpsideDown (boolean bool) {
+		//Sets UpSideDown to desired value
+		this.UpsideDown = bool;
+	}
 }
