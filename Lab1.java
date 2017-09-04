bc4c3b5c67691f208f0cbc4bf33a1464493ae356
olive <olive@DESKTOP-FA91UF5>
2017-09-04 18:42:21 +0200

    No duplicates, still string

--- Lab1.java
+++ Lab1.java
@@ -4,6 +4,7 @@ public class Lab1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RecursivePascal triangle = new RecursivePascal();
+		triangle.SetUpsideDown(false);
 		triangle.printPascal(5);
 	}
 
